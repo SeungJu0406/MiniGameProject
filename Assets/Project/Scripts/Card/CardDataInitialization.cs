a2ae0970c07d8c466c8217475bda421299f02e5b
SeungJu0406 <nss8164@gmail.com>
2024-09-29 02:33:32 +0900

    feat : 카드 코인 UI 구현

--- Assets/Project/Scripts/Card/CardDataInitialization.cs
+++ Assets/Project/Scripts/Card/CardDataInitialization.cs
@@ -9,15 +9,31 @@ public class CardDataInitialization : MonoBehaviour
     [SerializeField] GameObject nameTag;
     [SerializeField] SpriteRenderer cardIcon;
     [SerializeField] TextMeshProUGUI cardName;
+    [SerializeField] TextMeshProUGUI price;
     [SerializeField] CardModel model;
-
+ 
     [ContextMenu("Init")]
     void Init()
     {
-        nameTag.name = $"{data.id}. {data.engName}";
-        cardIcon.sprite = data.cardIcon;
-        cardName.text = data.cardName;
-        model = GetComponent<CardModel>();
-        model.data = data;
+        float fontSize = 0.2f;
+        if (nameTag != null) nameTag.name = $"{data.id}. {data.engName}";
+        if (cardIcon != null) cardIcon.sprite = data.cardIcon;
+        if (cardName != null) 
+        {
+            cardName.fontStyle = FontStyles.Bold;
+            cardName.fontSize = fontSize;
+            cardName.text = data.cardName;            
+        }
+        if (price != null) 
+        {
+            price.fontStyle = FontStyles.Bold;
+            price.fontSize = fontSize;
+            price.text = $"{data.price}";
+        }
+        if (model != null)
+        {
+            model = GetComponent<CardModel>();
+            model.data = data;
+        }
     }
 }
